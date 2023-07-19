d14bf3275d9f74a36fed84bb58afe060f86c6c04
Worlasi15 <efyawalasi@gmail.com>
2023-07-18 18:22:52 -0700

    replaces an element of a list at a specific position (like in C)

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+
+def element_at(my_list, idx):
+	for item in my_list
+    if idx < 0 or idx > (len(my_list) - 1):
+	    return None
+	elif idx == 0:
+	       return item
+	       idx -= 1
