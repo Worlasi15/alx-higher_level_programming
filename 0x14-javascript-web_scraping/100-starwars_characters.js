5174e84d7dc6543e7802b48e221671fc7692f3f3
Worlasi15 <efyawalasi@gmail.com>
2023-11-29 14:53:11 -0800

    print all characters of starwars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movieData = JSON.parse(body);
+    const charactersUrls = movieData.characters;
+
+    charactersUrls.forEach((characterUrl) => {
+      request.get(characterUrl, (characterError, characterResponse, characterBody) => {
+        if (characterError) {
+          console.error(characterError);
+        } else {
+          const characterData = JSON.parse(characterBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  }
+});
