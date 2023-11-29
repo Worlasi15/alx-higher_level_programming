9c9b6a0b172a3d4f78692ba7e854ac08ab5360e1
Worlasi15 <efyawalasi@gmail.com>
2023-11-29 15:08:11 -0800

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,40 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const movieData = JSON.parse(body);
-    const charactersUrls = movieData.characters;
-
-    // Using a Promise to handle asynchronous requests
-    function fetchCharacter(characterUrl) {
-      return new Promise((resolve, reject) => {
-        request.get(characterUrl, (characterError, characterResponse, characterBody) => {
-          if (characterError) {
-            reject(characterError);
-          } else {
-            const characterData = JSON.parse(characterBody);
-            resolve(characterData.name);
-          }
-        });
-      });
-    }
-
-    // Fetch all characters and print them
-    Promise.all(charactersUrls.map(fetchCharacter))
-      .then((characters) => {
-        characters.forEach((character) => {
-          console.log(character);
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
