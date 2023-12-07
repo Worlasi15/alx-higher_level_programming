941b0c2cc1e93b6a90481e9f1f549e810f312138
Worlasi15 <efyawalasi@gmail.com>
2023-12-06 16:40:03 -0800

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Wait for the document to be ready before executing the script
+document.addEventListener('DOMContentLoaded', function() {
+  // Select the <header> element and update its text color to red
+  var headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
