c5191e0640518762785f9e47354b8c0e971f8080
Worlasi15 <efyawalasi@gmail.com>
2023-12-06 16:01:52 -0800

    updates the text color

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Wait for the document to be ready before executing the script
+$(document).ready(function() {
+  // Select the <header> element and update its text color to red
+  $('header').css('color', '#FF0000');
+});
